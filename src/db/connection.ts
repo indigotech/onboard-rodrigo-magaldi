10c6496c619ca5e2b1a701bf3673b2eef380be1c
Rodrigo Magaldi <taqtile@Delirium.local>
2021-01-15 14:18:17 -0300

    Erase useless code

--- src/db/connection.ts
+++
@@ -1,7 +0,0 @@
-import { createConnection } from 'typeorm';
-
-export const connection = () => {
-  createConnection().then(() => {
-    return console.log("Connection to db successful");
-  })
-}
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,4 @@
 import 'reflect-metadata';
 import { setup } from "setup-server";
-// import { ApolloServer } from 'apollo-server';
-
-// import { connection } from 'db/connection';
-
-// import { typeDefs } from 'graphql/typeDefs';
-// import { resolvers } from 'graphql/resolvers';
-
-// export const setupServer = (): ApolloServer => {
-//   const server = new ApolloServer({ typeDefs, resolvers });
-//   return server;
-// }
-
-// export const startServer = async () => {
-//   connection();
-//   const server = setupServer();
-//   await server.listen(3030, () => console.log("Server listening on port 3030"));
-// }
-
-// startServer();
 
 setup();
