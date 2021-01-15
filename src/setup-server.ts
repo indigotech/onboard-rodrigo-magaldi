afaace02d45e4f6a66577b66b756d07352412556
Rodrigo Magaldi <taqtile@Delirium.local>
2021-01-15 19:45:58 -0300

    Remove ormconfig, hadle db connection via .env url

--- src/setup-server.ts
+++ src/setup-server.ts
@@ -1,15 +1,25 @@
 import { ApolloServer } from 'apollo-server';
+import { User } from 'entity/user';
 import { resolvers } from 'graphql/resolvers';
 import { typeDefs } from 'graphql/typeDefs';
 import { createConnection } from 'typeorm';
 
+import dotenv from 'dotenv';
+
 export async function setup() {
+  dotenv.config();
   await connectToDatabase();
   await runServer();
 }
 
 async function connectToDatabase() {
-  await createConnection();
+  await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: [User],
+    synchronize: true,
+    logging: false,
+  });
   console.log('Database connection successful');
 }
 
