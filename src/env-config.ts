4506906b724a67e92f53c0f9a376a1bb97cf4ba6
Rodrigo Magaldi <taqtile@Delirium.local>
2021-01-18 11:10:18 -0300

    Fix env config

--- src/env-config.ts
+++ src/env-config.ts
@@ -1,11 +1,5 @@
 import dotenv from 'dotenv';
 
 export function envConfig() {
-  console.log(process.env.ENV);
-
-  if (process.env.ENV === 'dev') {
-    dotenv.config({ path: '.env' });
-  } else if (process.env.ENV === 'test') {
-    dotenv.config({ path: '.env.test' });
-  }
+  dotenv.config({ path: process.env.ENV === 'test' ? '.env.test' : '.env' });
 }
