8f2fc909a14ff73cd1534d7d03a9d466b630bbf2
Rodrigo Magaldi <taqtile@Delirium.local>
2021-01-19 19:26:06 -0300

    Add multiple tests for user creation scenarios

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -74,6 +74,101 @@ describe('Login mutation test', async () => {
   });
 });
 
+describe('User creation mutation test', async () => {
+  before(async () => {
+    await addUserToDatabase('rodrigo', 'rodrigo@email.com', '01-01-1997', '12312312312', 'senha');
+  });
+
+  after(async () => {
+    await userRepository.clear();
+  });
+
+  it('Should create a new user in the database', async () => {
+    const authToken = await loginBeforeCreatingUser();
+
+    const userCreationMutation = buildUserCreationMutation(
+      'rafael',
+      'rafael@email.com',
+      '04-02-1995',
+      '12345678910',
+      'senha123',
+    );
+    const userCreationResponse = await request(requestUrl)
+      .post('/graphql')
+      .set('Authorization', authToken)
+      .send(userCreationMutation);
+
+    expect(userCreationResponse.body.data.createUser.user.name).to.equal('rafael');
+    expect(userCreationResponse.body.data.createUser.user.email).to.equal('rafael@email.com');
+    expect(userCreationResponse.body.data.createUser.user.birthDate).to.equal('04-02-1995');
+    expect(userCreationResponse.body.data.createUser.user.cpf).to.equal('12345678910');
+
+    const addedUser = await userRepository.findOne({
+      where: { email: 'rafael@email.com' },
+    });
+
+    expect(addedUser.name).to.equal('rafael');
+    expect(addedUser.email).to.equal('rafael@email.com');
+    expect(addedUser.birthDate).to.equal('04-02-1995');
+    expect(addedUser.cpf).to.equal('12345678910');
+
+    const numOfRows = await userRepository.count();
+    expect(numOfRows).to.equal(2);
+  });
+
+  it('Should return CustomError with message `JWT inválido.`', async () => {
+    const userCreationMutation = buildUserCreationMutation(
+      'rafael',
+      'rafael@email.com',
+      '04-02-1995',
+      '12345678910',
+      'senha123',
+    );
+    const userCreationResponse = await request(requestUrl).post('/graphql').send(userCreationMutation);
+
+    expect(userCreationResponse.body.errors[0].message).to.equal('JWT inválido.');
+    expect(userCreationResponse.body.errors[0].httpCode).to.equal(401);
+  });
+
+  it('Should return CustomError with message `E-mail já cadastrado.`', async () => {
+    const authToken = await loginBeforeCreatingUser();
+
+    const userCreationMutation = buildUserCreationMutation(
+      'nome',
+      'rodrigo@email.com',
+      '01-01-1999',
+      '32132132132',
+      'senha123',
+    );
+    const userCreationResponse = await request(requestUrl)
+      .post('/graphql')
+      .set('Authorization', authToken)
+      .send(userCreationMutation);
+
+    expect(userCreationResponse.body.errors[0].message).to.equal('E-mail já cadastrado.');
+    expect(userCreationResponse.body.errors[0].httpCode).to.equal(409);
+  });
+
+  it('Should return CustomError with message `Senha inadequada.`', async () => {
+    const authToken = await loginBeforeCreatingUser();
+
+    const userCreationMutation = buildUserCreationMutation(
+      'nome',
+      'novo@email.com',
+      '01-01-1999',
+      '32132132132',
+      'senha1',
+    );
+    const userCreationResponse = await request(requestUrl)
+      .post('/graphql')
+      .set('Authorization', authToken)
+      .send(userCreationMutation);
+
+    expect(userCreationResponse.body.errors[0].message).to.equal('Senha inadequada.');
+    expect(userCreationResponse.body.errors[0].httpCode).to.equal(403);
+  });
+});
+
 function checkJWT(token, rememberMe) {
   const verified = jwt.verify(token, process.env.JWT_SECRET);
   let expected: number;
@@ -113,4 +208,28 @@ function buildLoginMutation(email, password, rememberMe) {
     }
   `,
   };
+}
+
+function buildUserCreationMutation(name, email, birthDate, cpf, password) {
+  return {
+    query: `
+    mutation CreateUser {
+      createUser (name: "${name}", email: "${email}", birthDate: "${birthDate}", cpf: "${cpf}", password: "${password}") {
+        user {
+          id
+          name
+          email
+          birthDate
+          cpf
+        }
+      }
+    }
+    `,
+  };
+}
+
+async function loginBeforeCreatingUser() {
+  const loginMutation = buildLoginMutation('rodrigo@email.com', 'senha', false);
+  const loginResponse = await request(requestUrl).post('/graphql').send(loginMutation);
+  return loginResponse.body.data.login.token;
 }
