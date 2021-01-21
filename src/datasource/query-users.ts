a8694b80d502742ed8551b6d655c093ce0ec542b
Rodrigo Magaldi <taqtile@Delirium.local>
2021-01-21 18:19:00 -0300

    Implement error handling for invalid limit and offset in user list query

--- src/datasource/query-users.ts
+++ src/datasource/query-users.ts
@@ -1,7 +1,21 @@
 import { User } from 'entity/user';
+import { CustomError } from 'error/errors';
 import { getRepository } from 'typeorm';
 
 export const queryUsers = async (limit: number, offset: number) => {
+  if (limit < 0) {
+    throw new CustomError('Limite inválido.', 400, 'Não é possível realizar a query com limite negativo.');
+  }
+
+  if (offset < 0) {
+    throw new CustomError('Offset inválido.', 400, 'Não é possível realizar a query com offset negativo.');
+  }
+
+  const count = await getRepository(User).count();
+
+  const hasPreviousPage = offset === 0 ? false : true;
+  const hasNextPage = offset + limit < count ? true : false;
+
   const users = await getRepository(User).find({
     order: {
       name: 'ASC',
@@ -9,11 +23,6 @@ export const queryUsers = async (limit: number, offset: number) => {
     take: limit,
     skip: offset,
   });
-
-  const count = await getRepository(User).count();
-
-  const hasPreviousPage = offset === 0 ? false : true;
-  const hasNextPage = offset + limit < count ? true : false;
 
   return { users, count, hasNextPage, hasPreviousPage };
 };
