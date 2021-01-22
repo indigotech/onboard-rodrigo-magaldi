253bfd088955a09212d24e18aa87219cd1c19aa5
Rodrigo Magaldi <taqtile@Delirium.local>
2021-01-22 14:06:48 -0300

    Implement address table and relation

--- src/setup-server.ts
+++ src/setup-server.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from 'apollo-server';
-import { User } from 'entity/user';
 import { resolvers } from 'graphql/resolvers';
 import { typeDefs } from 'graphql/typeDefs';
 import { createConnection } from 'typeorm';
 import { envConfig } from 'env-config';
 import { formatError } from 'error/errors';
+import { Address } from 'entity/address';
+import { User } from 'entity/user';
 
 export async function setup() {
   envConfig();
@@ -17,7 +18,7 @@ export async function connectToDatabase() {
     await createConnection({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [User],
+      entities: [User, Address],
       synchronize: true,
       logging: false,
     });
